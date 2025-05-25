6d6eadcb82ee6359f083cae7919183bffbd841a4
Revathigireesh <revathigireesh04@gmail.com>
2025-05-25 14:04:31 +0530

    Initial commit of GreenBasket Spring Boot project

---
+++ src/main/java/com/example/GreenBascket/GreenBascketApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenBascket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBascketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBascketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GreenBascket/GreenBascketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenBascket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBascketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
